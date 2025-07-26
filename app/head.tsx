880ae0ffe9ac476f836b78fe942fb422afed1da0
Ashutosh Agarwal <ashutosh1503k@gmail.com>
2025-07-26 10:32:07 +0530

    added fav

--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,7 @@
 export default function Head() {
   return (
     <>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <link rel="icon" href="/favicon.png" type="image/png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>

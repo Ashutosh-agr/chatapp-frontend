7b74e3b887c227899bfbe4769eb81bb894683cca
Ashutosh Agarwal <ashutosh1503k@gmail.com>
2025-07-26 10:13:37 +0530

    Added favicon

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" href="/fevicon.png" type="image/png" />
+      <link rel="icon" href="/favicon.png" type="image/png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
   )
